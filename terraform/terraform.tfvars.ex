2c656a8b12020e9a376342ac90c36a5c538dc9c9
Seng Ye Xiong <sengye.xiong@gmail.com>
2019-05-17 07:11:45 +0000

    3rd commit

---
+++ terraform/terraform.tfvars.ex
@@ -0,0 +1,10 @@
+project_id = "gke-project-238703"
+
+region = "us-central1"
+
+name = "my-cluster"
+description = "Test cluster to showcase CI/CD with k8s and Helm"
+zone = "us-central1-a"
+initial_node_count = 3 # number of nodes in the cluster
+
+ntw_name = "default" # VPC network name which will be created
